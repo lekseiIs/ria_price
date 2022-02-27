d8d42bc7dc80a3765d2088dd5e77183db4f973b0
lekseiIs <liberateskeptic28@gmail.com>
2022-02-27 13:05:52 +0200

    Додав селект для моделей

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,13 +1,20 @@
 <template>
   <form action="#" class="search-form">
-    <select v-model="option" @change="test" name="auto-type" id="auto-type">
-      <option disabled value="">Выберите один из вариантов</option>
-      <option
-        v-for="option in options"
-        :key="option.value"
-        :value="option.value"
-      >
-        {{ option.name }}
+    <select
+      v-model="marka"
+      @change="fetchModels"
+      name="select-marka"
+      id="select-marka"
+    >
+      <option disabled value="" selected>Оберіть</option>
+      <option v-for="marka in marks" :key="marka.value" :value="marka.value">
+        {{ marka.name }}
+      </option>
+    </select>
+    <select name="select-model" id="select-model">
+      <option disabled value="" selected>Оберіть</option>
+      <option v-for="model in models" :key="model.value" :value="model.value">
+        {{ model.name }}
       </option>
     </select>
   </form>
@@ -18,8 +25,10 @@ export default {
   name: 'SearchForm',
   data() {
     return {
-      options: [],
-      option: '',
+      marks: [],
+      marka: '',
+      models: [],
+      model: '',
     };
   },
   mounted() {
@@ -27,24 +36,37 @@ export default {
       'http://api.auto.ria.com/categories/1/marks?api_key=U7i4BeQMgsVW0z4r9OxQvHc4H7C1IecipE3kX5zu',
     )
       .then((data) => {
-        const json = data.json();
-        return json;
+        const results = data.json();
+        return results;
       })
       .then((data) => {
-        this.options = data;
+        this.marks = data;
       })
       .catch((error) => {
         console.log(error);
-      })
-      .finally(() => console.log(this.options));
+      });
   },
   methods: {
-    test() {
-      console.log(this.option);
+    fetchModels() {
+      fetch(
+        `http://api.auto.ria.com/categories/1/marks/${this.marka}/models?api_key=U7i4BeQMgsVW0z4r9OxQvHc4H7C1IecipE3kX5zu`,
+      )
+        .then((data) => {
+          const results = data.json();
+          return results;
+        })
+        .then((data) => {
+          this.models = data;
+        })
+        .then(() => {
+          console.log(this.models);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
 </script>
 <style scoped>
-
 </style>
