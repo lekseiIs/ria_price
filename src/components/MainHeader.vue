88ac3e8327619ea966c1c43e065cc319ee4dd9ac
soboli89 <84281763+soboli89@users.noreply.github.com>
2022-04-01 12:09:44 +0300

    updated headder

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -4,15 +4,23 @@
       <div class="container">
         <div class="header__inner">
           <div class="header__top">
-            <img class="logo" src="../assets/logo.svg" alt="logo">
+            <div class="logo">
+                <img  src="../assets/logo2.svg" alt="logo">
+            </div>
+            <button class="btn" href="#">
+              <span class="plus">+ <span class="sell">Продати авто</span></span>
+            </button>
           </div>
           <div class="header__bottom">
-            <span class="header__message">
-                Слава Україні! Окупанти – геть!
-            </span>
+             <div class="header__message">
+               Слава Україні! Окупанти – геть!
           </div>
+          </div>
+
         </div>
+
       </div>
+
     </header>
   </div>
 </template>
@@ -29,31 +37,67 @@ export default {
   width: 100%;
 
   &__inner {
-    padding-top: 7px;
-    padding-left: 11px;
-    display: flex;
-    flex-direction: column;
+    padding-top: 16px;
+    //  padding-left: 11px;
+    //flex-direction: column;
   }
 
   &__top {
-    display: flex;
+   // display: flex;
     justify-content: space-between;
-    margin-bottom: 7px;
+    //margin-bottom: 7px;
     user-select: none;
+    height: 48px;
+    display: flex;
+    justify-content: space-around;
   }
-
-  &__message {
-    color: #256799;
-    font-size: 16px;
-    user-select: none;
+  &__bottom{
+    align-content: center;
   }
 }
+.container{
+    width: 80%;
+    margin-left: 10%;
+  }
+.logo img{
+      width: 163px;
+      height: 48px;
 
+      top: 16px;
+    }
+.header__message {
+  color: #256799;
+  font-size: 16px;
+  user-select: none;
+  width: 100%;
+  padding-left: 11px;
+}
+.btn {
+  width: 165px;
+  height: 47.79px;
+  background-color: #89BC39;
+  border-radius: 3px;
+
+}
+.plus{
+  font-size: 36px;
+  line-height: 42px;
+  color: #FFFFFF;
+  text-align: center;
+  font-weight: bold;
+}
+.sell{
+  font-size: 18px;
+  color: #FFFFFF;
+  font-weight: normal;
+}
+
+/*
 @media (min-width: 600px) and (max-width: 1400px) {
   .header{
     &__inner{
-      justify-content: center;
-      align-items: center;
+      justify-content: space-around;
+      //align-items: center;
     }
     &__message{
       font-size: 22px;
@@ -80,5 +124,5 @@ export default {
     width: 272px;
   }
 }
-
+*/
 </style>
