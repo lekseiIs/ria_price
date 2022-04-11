db9552370d6a0fa9acebef8b852e2bb1f1e42872
lekseiIs <liberateskeptic28@gmail.com>
2022-04-11 09:23:40 +0300

    Add font preconnect link

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+// @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
 
 body {
   margin: 0;
