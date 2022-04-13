df0013529c7152fd65b68b30b2b0534c0147669e
lekseiIs <liberateskeptic28@gmail.com>
2022-04-13 16:28:50 +0300

    Delete all country option

--- src/components/FormAdditionals.vue
+++ src/components/FormAdditionals.vue
@@ -8,7 +8,6 @@
           v-model="getFormState.state_id"
         >
           <option disabled value="" selected>Регіон</option>
-          <option value="" >Вся Україна</option>
           <option
             v-for="region in getRegions"
             :key="region.value"
